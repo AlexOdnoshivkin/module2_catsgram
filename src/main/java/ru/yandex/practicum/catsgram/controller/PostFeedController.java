63b8ff1e2dc7318ab1b6ef43fa0fed5d09ffa580
Alex <alexander.odnoshivkin@yandex.ru>
2022-07-09 17:54:54 +0300

    feat: добавлен обработчик ошибок и логирование в контроллер PostFeedController

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 public class PostFeedController {
   private final PostService postService;
@@ -23,9 +24,16 @@ public class PostFeedController {
 
   @PostMapping(value = "/feed/friends")
   public List<Post> postFriendsPosts(@RequestBody String body) throws JsonProcessingException {
+    log.debug("Получен запрос на просмотр последних постов друзе {}", body);
     ObjectMapper objectMapper = new ObjectMapper();
-    String stringFriendsPostParam = objectMapper.readValue(body, String.class);
-    FriendsPostsParam friendsPostsParam = objectMapper.readValue(stringFriendsPostParam, FriendsPostsParam.class);
+    FriendsPostsParam friendsPostsParam;
+    try {
+        String stringFriendsPostParam = objectMapper.readValue(body, String.class);
+        friendsPostsParam = objectMapper.readValue(stringFriendsPostParam, FriendsPostsParam.class);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Ошибка конвертации данных, проверьте корректность json", e);
+    }
+
     return postService.findFriendsPosts(friendsPostsParam);
   }
 }
