d77dfd1c70b034fc41707fd78e866d70c1045d14
Alex Odnoshivkin <alexander.odnoshivkin@yandex.ru>
2022-07-31 20:34:00 +0300

    feat:hack database

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,10 +1,26 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.service.HackCatService;
 
 @RestController
 public class SimpleController {
+
+    private final HackCatService hackCatService;
+
+    // внедрите бин HackCatService
+   @Autowired
+    public SimpleController(HackCatService hackCatService) {
+        this.hackCatService = hackCatService;
+    }
+
+    @GetMapping("/do-hack")
+    public void doHack(){
+        // хакните этих котиков
+        hackCatService.doHackNow();
+    }
 
     @GetMapping("/home")
     public String homePage() {
